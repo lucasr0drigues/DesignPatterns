a1f5b9202c2038d0b1fb3bc400bc557320d4ac3b
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-07-10 03:01:30 -0300

    factory method implementation

---
+++ CreationalPatterns/FactoryMethod/ConcreteProduct1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    // Concrete Products provide various implementations of the Product
+    // interface.
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct1}";
+        }
+    }
+}
