7d2c681d4c9729ba60edea948657a62f4d19801f
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2025-12-12 22:33:25 -0300

    Add Prototype Implementation

---
+++ CreationalPatterns/Prototype/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person) MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person) MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = Name;
+            return clone;
+        }
+    }
+}
--- CreationalPatterns/Prototype/Program.cs
+++ CreationalPatterns/Prototype/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Prototype;
+
+Person p1 = new Person();
+p1.Age = 42;
+p1.BirthDate = Convert.ToDateTime("1977-01-01");
+p1.Name = "Jack Daniels";
+p1.IdInfo = new IdInfo(555);
+
+Person p2 = p1.ShallowCopy();
+
+Person p3 = p1.DeepCopy();
+
+Console.WriteLine("Original values of p1, p2, p3: ");
+Console.WriteLine(" p1 instance values: ");
+DisplayValues(p1);
+Console.WriteLine(" p2 instance values: ");
+DisplayValues(p2);
+Console.WriteLine(" p3 instance values: ");
+DisplayValues(p3);
+
+Console.WriteLine();
+
+p1.Age = 32;
+p1.BirthDate = Convert.ToDateTime("1900-01-01");
+p1.Name = "Frank";
+p1.IdInfo.IdNumber = 7878;
+
+Console.WriteLine("Values of p1, p2, p3 after changes to p1: ");
+Console.WriteLine(" p1 instance values: ");
+DisplayValues(p1);
+Console.WriteLine(" p2 instance values (reference values have changed): ");
+DisplayValues(p2);
+Console.WriteLine(" p3 instance values (everything was kept the same): ");
+DisplayValues(p3);
+
+
+static void DisplayValues(Person p)
+{
+    Console.WriteLine($"Name: {p.Name}, Age: {p.Age}, BirthDate: {p.BirthDate.ToShortDateString()}");
+    Console.WriteLine($"Id: {p.IdInfo.IdNumber}");
+}
